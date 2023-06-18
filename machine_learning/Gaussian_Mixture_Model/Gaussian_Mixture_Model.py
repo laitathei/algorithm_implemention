22242276a75916487d19e2dd00a381fd09fe797e
laitathei <56575156+laitathei@users.noreply.github.com>
2023-06-18 14:05:35 +0800

    Update Gaussian_Mixture_Model.py

--- machine_learning/Gaussian_Mixture_Model/Gaussian_Mixture_Model.py
+++ machine_learning/Gaussian_Mixture_Model/Gaussian_Mixture_Model.py
@@ -5,11 +5,6 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm, multivariate_normal
 from numpy import inf, linalg
 
-# reference
-# https://github.com/Ransaka/GMM-from-scratch/blob/master/GMM%20from%20scratch.ipynb
-# https://python-course.eu/machine-learning/expectation-maximization-and-gaussian-mixture-models-gmm.php
-# https://github.com/sitzikbs/gmm_tutorial
-
 class GMM(object):
     def __init__(self, X, K):
         self.data = X
